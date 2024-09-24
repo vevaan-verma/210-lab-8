28f476e3779d31a2fbf6ca53a6a5046906b68006
Vevaan <vevaan.s.verma@gmail.com>
2024-09-23 22:54:43 -0700

    Initial commit

---
+++ 210-lab-8.cpp
@@ -0,0 +1,14 @@
+// COMSC-120 | Lab 8 | Vevaan Verma
+using namespace std;
+#include <iostream>
+#include <string>
+
+// main() is the entry point of the program
+// arguments: none
+// returns: int - the exit code of the program
+int main() {
+
+	string output = "Hello, World!";
+	cout << output << endl;
+
+}
